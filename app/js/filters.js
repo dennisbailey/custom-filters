eefd0b86325399f9a46faf46be2278de8aaf3d10
Dennis Bailey <dennis.bailey@zayo.com>
2017-05-07 18:00:15 -0600

    All the great commits

--- app/js/filters.js
+++ app/js/filters.js
@@ -1,7 +1,8 @@
 //convert snake_case to kebab-case
 app.filter('kebab', function () {
   return function(input){
-    return input
+    input = input || '';
+    return input.split('_').join('-')
   }
 });
 
@@ -9,7 +10,10 @@ app.filter('kebab', function () {
 //Returns an acronym
 app.filter('acronym', function() {
   return function(input){
-    return input
+    input = input || '';
+    return input.split(" ").map(function(el) {
+      return el[0];
+    }).join('')
   }
 });
 
@@ -23,10 +27,22 @@ app.filter('aLtCaPs', function() {
 
 
 //Use with ng-repeat and filter the collection
-app.filter('startsWithA', function () {
+app.filter('startsWithM', function () {
   return function(input) {
-    return input
+      if (input.charAt(0) === 'M') {
+        return input;
+      } 
   }
+});
+
+app.filter('startsWithZ', function () {
+  return function(input) {
+    console.log(arguments);
+    return input
+//     return input.map(function(el){
+//       return {name: el.name.toUpperCase()}
+//     })
+  } 
 });
 
 //Use with ng-repeat and an input to search a collection for string match
